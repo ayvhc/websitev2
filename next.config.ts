f01a4e163503b0bc639a014552c81f2e30017c33
[39vclac] <[chen773840@sas.edu.sg]>
2026-08-08 21:11:53 +0800

    Make site deploy on Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    cpus: 1,
+  },
+  turbopack: {
+    root: __dirname,
+  },
 };
 
 export default nextConfig;
